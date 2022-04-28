cd02fb454ee9b1abb3673c132a18e33262e8d6d3
Shalior <aliali.t2y@gmail.com>
2022-04-28 18:43:38 +0430

    Run config package

---
+++ src/FilamentPersianDateField.php
@@ -0,0 +1,7 @@
+<?php
+
+namespace Shalior\FilamentPersianDateField;
+
+class FilamentPersianDateField
+{
+}
--- src/FilamentPersianDateFieldServiceProvider.php
+++ src/FilamentPersianDateFieldServiceProvider.php
@@ -1,12 +1,12 @@
 <?php
 
-namespace VendorName\Skeleton;
+namespace Shalior\FilamentPersianDateField;
 
 use Spatie\LaravelPackageTools\Package;
 use Spatie\LaravelPackageTools\PackageServiceProvider;
-use VendorName\Skeleton\Commands\SkeletonCommand;
+use Shalior\FilamentPersianDateField\Commands\FilamentPersianDateFieldCommand;
 
-class SkeletonServiceProvider extends PackageServiceProvider
+class FilamentPersianDateFieldServiceProvider extends PackageServiceProvider
 {
     public function configurePackage(Package $package): void
     {
@@ -16,10 +16,7 @@ class SkeletonServiceProvider extends PackageServiceProvider
          * More info: https://github.com/spatie/laravel-package-tools
          */
         $package
-            ->name('skeleton')
-            ->hasConfigFile()
-            ->hasViews()
-            ->hasMigration('create_skeleton_table')
-            ->hasCommand(SkeletonCommand::class);
+            ->name('filament-persian-date-field')
+            ->hasViews();
     }
 }
